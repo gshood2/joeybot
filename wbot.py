dbb7c830a702b33f9bf2bf76637c408b19f0981c
Greg <gshood2@gmail.com>
2022-02-11 10:45:04 -0500

    disconnect

--- wbot.py
+++ wbot.py
@@ -1,9 +1,11 @@
 from nextcord.ext import commands
 import nextcord
 import ctypes
+import asyncio
 
 # joey id = 416567736739823619
 # my id = 308698142428364812
+
 wbot = commands.Bot(command_prefix='!')
 nextcord.opus.load_opus(ctypes.util.find_library("opus"))
 print("opus found = ", nextcord.opus.is_loaded())
@@ -15,11 +17,13 @@ async def on_ready():
 
 @wbot.event
 async def on_voice_state_update(member, before, after):
-    if member.id == 416567736739823619:
+    targetMember=308698142428364812
+    if member.id == targetMember:
         print(member.name)
         channel = member.voice.channel
         whiteboy = await channel.connect()
         whiteboy.play(nextcord.FFmpegPCMAudio('whiteboy.mp3')) 
-          
+        await asyncio.sleep(6)
+        await whiteboy.disconnect()
 
 wbot.run('OTQxMzg4NTQ2NjI1OTEyOTcy.YgVOZw.9rzxm8zzL3nAw9MMnWPH81mXZpk')
