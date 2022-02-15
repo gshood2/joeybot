d927314a271ff49db637b146b214adeeba184585
Greg <gshood2@gmail.com>
2022-02-15 02:19:23 -0500

    wbot v1

--- wbot.py
+++ wbot.py
@@ -1,29 +1,35 @@
 from nextcord.ext import commands
 import nextcord
 import ctypes
+import os
 import asyncio
-
-# joey id = 416567736739823619
-# my id = 308698142428364812
-
+from dotenv import load_dotenv
+#load .env
+load_dotenv()
+#sets target
+targetMember = int(os.getenv('TARGET')) 
+#bot setup + opus lib check
 wbot = commands.Bot(command_prefix='!')
 nextcord.opus.load_opus(ctypes.util.find_library("opus"))
 print("opus found =", nextcord.opus.is_loaded())
 
 @wbot.event
 async def on_ready():
-    print(f'Logged in as {wbot.user}')
-
+    print(f'Logged in as {wbot.user.name}\n' + 'Connected to:')
+    guilds = wbot.guilds
+    for server in guilds:
+        print(server.name)
 
 @wbot.event
 async def on_voice_state_update(member, before, after):
-    targetMember=416567736739823619
-    if member.id == targetMember:
-        print(member.name)
+
+    if before.channel is None and member.id == targetMember:
+        #print(member.name + " has joined")
         channel = member.voice.channel
+        await asyncio.sleep(1)
         whiteboy = await channel.connect()
         whiteboy.play(nextcord.FFmpegPCMAudio('whiteboy.mp3')) 
-        await asyncio.sleep(6)
+        await asyncio.sleep(4)
         await whiteboy.disconnect()
 
-wbot.run('OTQxMzg4NTQ2NjI1OTEyOTcy.YgVOZw.9rzxm8zzL3nAw9MMnWPH81mXZpk')
+wbot.run(os.getenv('TOKEN'))
