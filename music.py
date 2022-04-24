39869a5c8d0d21ebdea7e856bb75c38dfb8709b1
gshood2 <85319501+gshood2@users.noreply.github.com>
2022-04-24 17:36:28 -0400

    improve music, moved leave

--- music.py
+++ music.py
@@ -14,18 +14,23 @@ class queue():
 
 
 class Music(commands.Cog):
-    @commands.command()
-    async def leave(self,ctx):
-        await ctx.voice_client.disconnect()
-        await ctx.send("leaving voice channel")
     
     @commands.command(name='play',brief='play a video')
-    async def play(self,ctx,url):
-        ydl_opts = {'format': 'bestaudio',}
-        channel = await ctx.message.author.voice.channel.connect()
-        with YoutubeDL(ydl_opts) as ydl:
-            video_info = ydl.extract_info(url,download=False)
-        channel.play(nextcord.FFmpegPCMAudio(video_info['url']))
+    async def play(self,ctx,url): 
+        if ctx.message.author.voice is None:
+            await ctx.send("Please join a voice channel")
+        else:
+            ydl_opts = {'format': 'bestaudio',}
+            channel = ctx.message.author.voice.channel 
+            vc = nextcord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+            if vc is None:
+                voice = await channel.connect()
+            else:
+                await ctx.voice_client.disconnect()
+                voice = await channel.connect()
+            with YoutubeDL(ydl_opts) as ydl:
+                video_info = ydl.extract_info(url,download=False)
+            voice.play(nextcord.FFmpegPCMAudio(video_info['url']))
 
 def setup(joeybot):
     joeybot.add_cog(Music(joeybot))
--- wbot.py
+++ wbot.py
@@ -49,12 +49,12 @@ async def on_voice_state_update(member, before, after):
                 pain.play(nextcord.FFmpegOpusAudio(FILE))
                 await asyncio.sleep(4)
                 await pain.disconnect()
-    else: print("join not active")
+    else: print("Join not active")
 #Main CMD COG
 class Main(commands.Cog, name='Main'):
     def __init__(self, bot):
         self.bot = joeybot
-    @commands.command(name='bomb',brief='bomb voice channel')
+    @commands.command(name='bomb',brief='Bomb voice channel')
     async def bomb(self,ctx,target,filename):
             Guild=ctx.message.guild
             tfile = filename + ".opus"
@@ -67,15 +67,20 @@ class Main(commands.Cog, name='Main'):
                     await asyncio.sleep(10)
                     await bomb.disconnect()
 
-    @commands.command(name='togglejoin', brief='toggles pain for joey and jack')
+    @commands.command(name='togglejoin', brief='Toggles autojoin bomb')
     async def togglejoey(self,ctx):
         global Join
         if Join == False:
             Join=True
-            await  ctx.send("Join is now enabled")
+            await ctx.send("Join is now enabled.")
         elif Join == True:
             Join=False
-            await  ctx.send("Join is now disabled")
+            await ctx.send("Join is now disabled.")
+
+    @commands.command(brief='Makes bot leave the channel')
+    async def leave(self,ctx):
+        await ctx.voice_client.disconnect()
+        await ctx.send("Leaving")
     
 joeybot.load_extension('music')   
 joeybot.add_cog(Main(joeybot))
